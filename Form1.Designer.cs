c56ae1e3b93d3b6ad267c4b793c2c6af4bec066b
TobiasWust <kontakt@tobiaswust.de>
2020-05-23 17:35:45 +0200

    v0.1

---
+++ Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace CondorMapMover
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.condorpathEl = new System.Windows.Forms.TextBox();
+            this.condorpathButton = new System.Windows.Forms.Button();
+            this.condorpathFile = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.newpathEl = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.newpathFile = new System.Windows.Forms.FolderBrowserDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.go = new System.Windows.Forms.Button();
+            this.logEl = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(16, 170);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.listBox1.Size = new System.Drawing.Size(354, 95);
+            this.listBox1.TabIndex = 0;
+            // 
+            // condorpathEl
+            // 
+            this.condorpathEl.Location = new System.Drawing.Point(16, 44);
+            this.condorpathEl.Name = "condorpathEl";
+            this.condorpathEl.Size = new System.Drawing.Size(211, 20);
+            this.condorpathEl.TabIndex = 1;
+            this.condorpathEl.TextChanged += new System.EventHandler(this.condorpathEl_TextChanged);
+            // 
+            // condorpathButton
+            // 
+            this.condorpathButton.Location = new System.Drawing.Point(233, 44);
+            this.condorpathButton.Name = "condorpathButton";
+            this.condorpathButton.Size = new System.Drawing.Size(137, 23);
+            this.condorpathButton.TabIndex = 2;
+            this.condorpathButton.Text = "Choose Directory";
+            this.condorpathButton.UseVisualStyleBackColor = true;
+            this.condorpathButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(193, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "1. Condor Install Path?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(225, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "2. New Directory for Maps?";
+            // 
+            // newpathEl
+            // 
+            this.newpathEl.Location = new System.Drawing.Point(16, 112);
+            this.newpathEl.Name = "newpathEl";
+            this.newpathEl.Size = new System.Drawing.Size(211, 20);
+            this.newpathEl.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(233, 112);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Choose Directory";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(136, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "3. Which Maps?";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 280);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(271, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "4. Move Files and create Symlink";
+            // 
+            // go
+            // 
+            this.go.Location = new System.Drawing.Point(12, 303);
+            this.go.Name = "go";
+            this.go.Size = new System.Drawing.Size(358, 23);
+            this.go.TabIndex = 9;
+            this.go.Text = "Go";
+            this.go.UseVisualStyleBackColor = true;
+            this.go.Click += new System.EventHandler(this.go_Click);
+            // 
+            // logEl
+            // 
+            this.logEl.BackColor = System.Drawing.Color.FloralWhite;
+            this.logEl.Location = new System.Drawing.Point(401, 44);
+            this.logEl.Multiline = true;
+            this.logEl.Name = "logEl";
+            this.logEl.ReadOnly = true;
+            this.logEl.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.logEl.Size = new System.Drawing.Size(369, 282);
+            this.logEl.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(397, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "5. Progress";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(742, 340);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(28, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "v0.1";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 340);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(58, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Created by";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(277, 340);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(47, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Code on";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(78, 339);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(67, 13);
+            this.linkLabel1.TabIndex = 15;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Tobias Wust";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(330, 340);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(38, 13);
+            this.linkLabel2.TabIndex = 16;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "Github";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(787, 367);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.logEl);
+            this.Controls.Add(this.go);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.newpathEl);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.condorpathButton);
+            this.Controls.Add(this.condorpathEl);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Condor MapMover";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox condorpathEl;
+        private System.Windows.Forms.Button condorpathButton;
+        private System.Windows.Forms.FolderBrowserDialog condorpathFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox newpathEl;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog newpathFile;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button go;
+        private System.Windows.Forms.TextBox logEl;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace CondorMapMover
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string condorPath = (string)Registry.GetValue("HKEY_CLASSES_ROOT\\cndr2\\DefaultIcon", "", "Condor not found");
+            if (condorPath != null) condorPath = condorPath.Replace("\\Condor.exe", "");
+            condorpathEl.Text = condorPath;
+            newpathEl.Text = Directory.GetCurrentDirectory();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (condorpathFile.ShowDialog() == DialogResult.OK)
+            {
+                condorpathEl.Text = condorpathFile.SelectedPath;
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (newpathFile.ShowDialog() == DialogResult.OK)
+            {
+                newpathEl.Text = newpathFile.SelectedPath;
+            }
+        }
+
+        private void condorpathEl_TextChanged(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            string mapPath = condorpathEl.Text + "\\landscapes\\";
+            if (!Directory.Exists(mapPath)) return;
+
+            string[] subdirectoryEntries = Directory.GetDirectories(mapPath);
+            foreach (string subdirectory in subdirectoryEntries)
+            {
+                string map = subdirectory.Replace(mapPath, "");
+                listBox1.Items.Add(map);
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void log(string text)
+        {
+            logEl.AppendText(text + "\r\n");
+        }
+
+        private void go_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count == 0)
+            {
+                log("error: no maps found in this location");
+                return;
+            }
+
+            if (!Directory.Exists(newpathEl.Text))
+            {
+                log("error: target directory not found");
+                return;
+            }
+
+            if (listBox1.SelectedItems.Count == 0)
+            {
+                log("select at least one map");
+                return;
+            }
+
+            foreach (string map in listBox1.SelectedItems) moveMap(map);
+
+            log("done");
+        }
+
+        private void moveMap(string map)
+        {
+            log("copy map " + map + "please wait...");       
+
+            string sourceDirectory = condorpathEl.Text + @"\landscapes\" + map;
+            string destinationDirectory = newpathEl.Text + @"\" + map;
+            try
+            {
+                DirectoryCopy(sourceDirectory, destinationDirectory, true);
+                log("finished copying " + map);
+}
+            catch (Exception e)
+            {
+                log(e.Message);
+            }
+
+            log("delete source map");
+            try
+            {
+                Directory.Delete(sourceDirectory, true);
+                log("deleted " + sourceDirectory);
+            }
+            catch (Exception e)
+            {
+                log(e.Message);
+            }
+
+            log("creating mklink");
+            makeMklink(sourceDirectory, destinationDirectory);
+        }
+
+        private void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+        {
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + sourceDirName);
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            // If the destination directory doesn't exist, create it.
+            if (!Directory.Exists(destDirName))
+            {
+                Directory.CreateDirectory(destDirName);
+            }
+
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                log("copy file " + file);
+                string temppath = Path.Combine(destDirName, file.Name);
+                file.CopyTo(temppath, false);
+            }
+
+            // If copying subdirectories, copy them and their contents to new location.
+            if (copySubDirs)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string temppath = Path.Combine(destDirName, subdir.Name);
+                    DirectoryCopy(subdir.FullName, temppath, copySubDirs);
+                }
+            }
+        }
+
+        private void makeMklink(string source, string dest)
+        {
+            var psi = new ProcessStartInfo("cmd.exe", " /C mklink /j \"" + source + "\" \"" + dest + "\"");
+            psi.CreateNoWindow = true;
+            psi.UseShellExecute = false;
+            Process.Start(psi).WaitForExit();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://www.tobiaswust.de");
+        }
+    }
+}
